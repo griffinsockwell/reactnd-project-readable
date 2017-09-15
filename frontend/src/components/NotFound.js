59c1191ee29199843e8d920307f9791c291be651
griffinsockwell <gs71312@icloud.com>
2017-09-14 23:21:00 -0400

    Improve route not found

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledNotFound = styled.div``;
+const StyledNotFound = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 40px;
+  font-weight: 700;
+  color: #9b9b9b;
+`;
 
 const NotFound = () => <StyledNotFound>Route not found</StyledNotFound>;
 
